0e50e62ec3b1c3d85720052c23042eec5f54c464
Juan Palomino <Jpalomino09@gmail.com>
2019-07-05 04:38:36 -0700

    logic finished

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,19 +1,24 @@
-
-var count = 3;
+var count = 10;
 var countDownInterval;
 
 function start() {
-    countDownInterval = setInterval(countdown, 1000);
+  count = 10;
+  countDownInterval = setInterval(countdown, 1000);
+  countdown();
+  $("#gameInstructions").css("display", 
+  "none");
+  $("#gameCounter").css("display", "block")
 }
 
-$("#timer").html(count);
+$(".timer").html(count);
 function countdown() {
-    count = count - 1;
-    $("#timer").html(count);
-    if (count <= 0) {
-        console.log("countdown over!!!");
-        clearInterval(countDownInterval);
-    }
+  count = count - 1;
+  $(".timer").html(count);
+  if (count <= 0) {
+    checkResponses();
+    console.log("countdown over!!!");
+    clearInterval(countDownInterval);
+  }
 }
 
 function checkIncorrect() {
@@ -23,7 +28,7 @@ function checkIncorrect() {
     for (var j = 0; j < radios.length; j++) {
       var radio = radios[j];
       if (radio.value == "incorrect" && radio.checked) {
-       incorrect++;
+        incorrect++;
       }
     }
   }
@@ -31,30 +36,45 @@ function checkIncorrect() {
 }
 
 function checkCorrect() {
-    var correct = 0;
-   for (var i = 1; i <= 45; i++) {
-     var radios = document.getElementsByName("group" + i);
-     for (var j = 0; j < radios.length; j++) {
-       var radio = radios[j];
-       if (radio.value == "correct" && radio.checked) {
-         correct++;
-       }
-     }
-   }
-   console.log("Correct Responses: " + correct);
- }
- 
-function checkResponses(){
-    checkCorrect();
-    checkIncorrect();
+  var correct = 0;
+  for (var i = 1; i <= 45; i++) {
+    var radios = document.getElementsByName("group" + i);
+    for (var j = 0; j < radios.length; j++) {
+      var radio = radios[j];
+      if (radio.value == "correct" && radio.checked) {
+        correct++;
+      }
+    }
+  }
+  switch (correct) {
+    case 0:
+      $("#results").html(
+        "Oh no you got " + correct + " questions correct! YOU SUCK"
+      );
+      break;
+    case 2:
+      $("#results").html("You answered " + correct + " questions correctly.");
+      break;
+    case 3:
+      $("#results").html(
+        "PERFECTENSCHLAG! You answered all " + correct + " questions correctly!"
+      );
+  }
+
+  console.log("Correct Responses: " + correct);
 }
-$("#testButton").click(() => {
+
+function checkResponses() {
+  checkCorrect();
+  checkIncorrect();
+}
+$("#start").click(() => {
   console.log("TESTING");
-  countdown();
+  
   start();
 });
 
-$("#submit").click(() =>{
-console.log("submitted!");
-checkResponses();
+$("#submit").click(() => {
+  console.log("submitted!");
+  checkResponses();
 });
